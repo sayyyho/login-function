07bb015915d5058f49dcb87743423108b41ad8f5
sayyyho <323psh@naver.com>
2022-07-07 17:44:49 +0900

    make public file

--- app/app.js
+++ app/app.js
@@ -25,7 +25,7 @@ const home = require("./src/routes/home"); //라우팅
 // 모듈 : app setting
 app.set("views", "./src/views"); 
 app.set("view engine", "ejs");
-
+app.use(express.static(`${__dirname}/src/public`))
 
 app.use("/", home); // use -> 미들웨어를 등록하는 메소드.
 
